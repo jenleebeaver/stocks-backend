85bb15b8282179716b634577f1e312b939653450
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-13 13:52:21 -0700

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :stocks
-  resources :portfolios
+
+  namespace :api do
+    namespace :v1 do
+      resources :stocks
+      resources :portfolios
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  fetch('http://localhost:3000/api/v1/portfolios')
 end
