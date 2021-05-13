ffad2484b63fea2897b0e5df15b47b624b44bab6
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-13 13:17:16 -0700

    Update 20210513200243_create_portfolios.rb

--- db/migrate/20210513200243_create_portfolios.rb
+++ db/migrate/20210513200243_create_portfolios.rb
@@ -1,8 +1,8 @@
 class CreatePortfolios < ActiveRecord::Migration[6.0]
   def change
     create_table :portfolios do |t|
-      t.String :s
-      t.Float :p
+      t.string :s
+      t.float :p
 
       t.timestamps
     end
