023097f9074b1ded8d06ae92f2ed2d09a74de7f4
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-13 13:26:17 -0700

    Add validations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,6 @@
 class Portfolio < ApplicationRecord
     has_many :stocks
+
+    validates :s, presence: true
+    validates :p, presence: true
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,7 @@
 class Stock < ApplicationRecord
     belongs_to :portfolio
+
+    validates :s, presence: true
+    validates :p, presence: true
+    validates :description, presence: true
 end
