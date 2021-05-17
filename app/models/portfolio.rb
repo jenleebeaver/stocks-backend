445e50efc55e452edde77fd462ff5de37700723f
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-17 14:08:49 -0700

    Update portfolio.rb
    models and validations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
     has_many :stocks
+    has_many :cryptoexchanges  
+    has_many :portfolio_stocks, :through :stocks
 
-    validates :s, :p, presence: true
+    validates :s, :p, :cryptoexchanges, presence: true
 end
