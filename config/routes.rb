a9e9e0ed211ee1c9b4624fb996e7d48f48474f8d
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-17 15:20:03 -0700

    Update routes.rb
    namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :stocks
       resources :portfolios
+      resources :stocks
+      resources :cryptoexchanges
+      resources :portfolio_stocks
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  fetch('http://localhost:3000/api/v1/portfolios')
+  # fetch('http://localhost:3000/api/v1/portfolios')
 end
