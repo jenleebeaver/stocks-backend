d7a2bf6c2c36446299fc3ebe0535c9a7cc137dfc
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-17 14:07:05 -0700

    stocks_db update

--- db/migrate/20210513201904_create_stocks.rb
+++ db/migrate/20210513201904_create_stocks.rb
@@ -4,9 +4,23 @@ class CreateStocks < ActiveRecord::Migration[6.0]
       t.string :s
       t.float :p
       t.string :description
+      t.string :country
+      t.string :currency
+      t.string :exchange
+      t.string :ipo
+      t.string :finnhubIndustry 
+      t.bigInt :marketCapitalization
+      t.array :peers
+      t.integer :buy
+      t.integer :hold
+      t.integer :sell
+      t.integer :strongBuy
+      t.integer :strongSell
+      t.float :peRatio 
       t.integer :portfolio_id
 
       t.timestamps
     end
   end
 end
+
