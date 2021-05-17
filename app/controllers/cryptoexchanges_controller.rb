8c309dd640dc1232da6cd6ddd5ef802e75f9cb52
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-17 16:46:07 -0700

    delete cryptoexchange table

--- app/controllers/cryptoexchanges_controller.rb
+++
@@ -1,2 +0,0 @@
-class CryptoexchangesController < ApplicationController
-end
--- app/models/cryptoexchange.rb
+++
@@ -1,6 +0,0 @@
-class Cryptoexchange < ApplicationRecord
-    belongs_to :portfolio
-    belongs_to :portfolio_stock
-
-    validations :symbol, :displaySymbol, :description, :portfolio_id, presence: true 
-end
