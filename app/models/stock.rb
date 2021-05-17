b58d281f36a5995f294f89be8b5db962b05f77b9
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-17 14:43:41 -0700

    Update stock.rb
    associations and validations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,23 @@
 class Stock < ApplicationRecord
     belongs_to :portfolio
+    belongs_to :portfolio_stock
 
-    validates :s, :p, :description, presence: true
+    validates :s, 
+            :p, 
+            :description, 
+            :country, 
+            :currency, 
+            :exchange, 
+            :ipo, 
+            :finnhubIndustry, 
+            :marketCapitalization, 
+            :peers, 
+            :buy, 
+            :hold, 
+            :sell, 
+            :strongBuy, 
+            :strongSell, 
+            :peRatio, 
+            :portfolio_id, 
+            presence: true
 end
