ec48b5a66b8c594cafafc314a5620af5cac1ac61
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-18 13:56:14 -0700

    change to PortfolioStocks
    portfolio_stocks to PortfolioStocks

--- app/models/portfolio_stock.rb
+++
@@ -1,6 +0,0 @@
-class PortfolioStock < ApplicationRecord
-    has_many :stocks
-    has_many :portfolios, :through :stocks
-
-    validations :portfolio_id, :stock_id,  :cryptoexchange_id, presence: true   
-end
