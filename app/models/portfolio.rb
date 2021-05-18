eae546268f5a8874af086cf684547ea044773333
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-18 13:58:50 -0700

    Update portfolio.rb

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
     has_many :stocks 
-    has_many :portfolio_stocks, :through :stocks
+    has_many :portfolio_stocks, through: :stocks
 
     validates :s, :p, presence: true
 end
