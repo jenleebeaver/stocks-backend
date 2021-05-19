8b60f9c00b2ec7c1f7cdb27570811d752f3c30be
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-19 10:46:17 -0700

    create CRUD actions

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::PortfoliosController < ApplicationController
+
+    def index
+        @portfolios = Portfolio.all 
+        render json: @portfolios
+    end
+
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
+        if @portfolio.save 
+            render json: @portfolio, status: :accepted
+        else
+            render json: { errors: @portfolio.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def show
+        find_by
+        render json:@portfolio
+    end
+
+    def destroy 
+        find_by
+        @portfolio.destroy
+    end 
+
+    private 
+
+    def portfolio_params
+        params.require(:portfolio).permit(:s, :p)
+    end
+
+    def find_by 
+        @portfolio = Portfolio.find_by(id: params[:id])
+    end
+
 end
--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::StocksController < ApplicationController
+    before_action :set_portfolio
+
+    def index
+        #this will route api/v1/portfolios/id/stocks
+        @stocks = @portfolio.stocks  
+        render json: @stocks
+    end
+
+    def show
+        find_by
+        render json: @stock
+    end
+
+    def create
+        @stock = @portfolio.stock.build(stock_params)
+        if @stock.save 
+            render json: @stock, status: :accepted
+        else
+            render json: { errors: @stock.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def destroy 
+        find_by
+        @stock.destroy
+    end 
+
+    private
+    
+    def set_portfolio
+         @portfolio = Portfolio.find_by(id: params[:portfolio_id])
+    end
+
+    def stock_params
+        params.require(:stock).permit(:s, :p, :description, :country, :currency, :exchange, :ipo, :finnhubIndustry, :marketCapitalization, :peers, :buy, :hold,:sell, :strongBuy, :strongSell, :peRatio, :portfolio_id, :portfolio_stock_id)
+    end
+
+    def find_by 
+        @stock = Stock.find_by(id: params[:id])
+    end
 end
