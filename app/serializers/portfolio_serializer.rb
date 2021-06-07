21f5152bcdf85a93ffc30daefcf7822f0360acf3
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-07 09:48:30 -0700

    Update portfolio_serializer.rb
    users -> user

--- app/serializers/portfolio_serializer.rb
+++ app/serializers/portfolio_serializer.rb
@@ -1,6 +1,6 @@
 class PortfolioSerializer < ActiveModel::Serializer
-  include FastJsonapi::ObjectSerializer
-  belongs_to: users
+  # include FastJsonapi::ObjectSerializer
+  belongs_to :user
 
   attributes :id, :portfolio_name, :user_id, :updated_at, :created_at
 end
